00136cc9bc063a2c49d2083e7e90276f0027f2cb
BirgitKRA <birgit@gmail.com>
2026-05-04 13:07:57 +0100

    actualizacionesd

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name":"UF1889_E2_ - Actividad comercial clientes",
+    "version":"1.0.0",
+    "category":"CRM",
+    "summary":"analasis de seguimientos comerciales por clientes",
+    "author": "Birgit KRA",
+    "license": "LPGL-3",
+    "depends":[
+        "base",
+        "mail",
+        "contacts",
+    ],
+    "data":[
+        "",
+    ],
+    "installable":True,
+    "application":False,
+}
